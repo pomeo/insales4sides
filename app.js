1ae56bba0b2f4ff0174871e79168d9b59c5df926
Sergey Ovechkin <me@sovechkin.com>
2013-12-02 19:13:29 +0400

    базовая работа с api через restler

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 var express = require('express')
   , http = require('http')
   , rest = require('restler')
-  , parser = require('libxml-to-js')
+  , xml2js = require('xml2js')
+  , util = require('util')
   , path = require('path');
 
 var app = express();
@@ -25,8 +26,23 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+app.get('/cities', function(req, res){
+  rest.get('http://www.4sides.ru/common/api/calculate.php?action=get_data').on('complete', function(result) {
+    if (result instanceof Error) {
+      console.log('Error: ' + result.message);
+    } else {
+      rest.parsers.xml(result, function(err, newData) {
+        res.send(newData);
+      });
+    }
+  });
+});
+
 app.get('/', function(req, res){
-  res.send('ok');
+  console.log('Регион : ' + req.query.region + ' Город : ' + req.query.city + ' Вес : ' + req.query.weight);
+  //var url = 'http://4sides.ru/common/api/calculate.php?action=calculate_rf&from=' + req.query + '&to=' + req.query + '&weight=' + req.query + '&obl=' + req.query;
+  res.setHeader('Content-Type', 'application/json');
+  res.send(req.query.callback + '({delivery_price: 100})');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
