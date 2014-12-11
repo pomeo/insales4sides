ab60393a929f4daebdc60651a8d32e66b8c2a84e
Sergey Ovechkin <me@sovechkin.com>
2014-12-11 21:50:54 +0300

    capistrano, переезжаем с сервера на DO

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,38 +4,20 @@ set :application, "m.insales.sovechkin.com"
 #========================
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
-
-default_run_options[:pty] = true
-default_run_options[:shell] = '/bin/bash'
 set :repository,  "git@github.com:pomeo/insales4sides.git"
-set :user,        "pomeo"
-set :port,        2222
-set :use_sudo,    false
-set :deploy_via,  :copy
-set :scm,         :git
-set :copy_exclude,["/.git/", "/.gitignore", "/Capfile", "/config/", "/config.yaml", "/Rakefile", "Rules", "/tmp/", "/mkmf.log"]
-
-set :supervisord_pidfile, "/var/run/supervisord.pid"
+set :deploy_to,   "/home/ubuntu/www/4sides"
 set :supervisord_start_group, "4sides"
 set :supervisord_stop_group, "4sides"
 #========================
 #ROLES
 #========================
-role :app,        "#{application}"
-set :deploy_to,   "/var/www/insales/4sides/www"
-set :deploy_user, "pomeo"
-set :deploy_group,"pomeo"
+role :app,        "ubuntu@#{application}"
 
-namespace :deploy do 
-  desc "Install node modules non-globally"
-  task :npm_install do
-    run ". /home/pomeo/.nvm/nvm.sh && cd #{current_path} && npm install"
-  end
-
+namespace :deploy do
   desc "Change node.js port"
   task :chg_port do
-    run "sed -i 's/3000/4010/g' #{current_path}/app.js"
+    run "sed -i 's/3000/9000/g' #{current_path}/app.js"
   end
 end
- 
+
 after "deploy:create_symlink", "deploy:npm_install", "deploy:chg_port", "deploy:restart"
