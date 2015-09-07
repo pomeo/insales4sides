753a326fb357d9e1f07ce4def7319c68ce1c5b20
Sergey Ovechkin <me@pomeo.me>
2015-09-07 16:31:59 +0400

    refactoring app.js

--- app.js
+++ app.js
@@ -1,319 +1,71 @@
+'use strict';
+const express    = require('express');
+const debug      = require('debug')('app');
+const bodyParser = require('body-parser');
+const logger     = require('morgan');
+const path       = require('path');
 
-/**
- * Module dependencies.
- */
+let routes = require('./routes/index');
 
-var express = require('express')
-  , http = require('http')
-  , rest = require('restler')
-  , xml2js = require('xml2js')
-  , util = require('util')
-  , fs = require('fs')
-  , async = require('async')
-  , csv = require('csv')
-  , path = require('path');
-
-var app = express();
+let app = express();
 
 var kladr_key = process.env.kladrkey;
 var kladr_token = process.env.kladrtoken;
 
-// all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+
+// all environments
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+if (app.get('env') !== 'development') {
+  app.enable('view cache');
+}
+app.set('trust proxy', 1);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+app.use('/', routes);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
-// основная функция для поиска городов и регионов
-var f = function(res, req, region, city, data) {
-  var jobj = JSON.parse(data);
-  var we = req.query.weight;
-  var n = 0;
-  var weightList = [];
-  var weightEqual = 0.1;
-  var price = '';
-  for (var weight in jobj[0].weight) {
-    weightList.push(parseFloat(weight.replace(',', '.')));
-  }
-  weightList = weightList.sort(function(a, b){ return a - b });
-  if (parseFloat(req.query.weight) < 0.1) {
-    we = 0.1;
-  }
-  // если вдруг вес будет больше максимального веса службы доставки
-  if (parseFloat(req.query.weight) > weightList[weightList.length-1]) {
-    we = weightList[weightList.length-1];
-  }
-  // возвращаем +1 вес от текущего в шкале службы доставки
-  async.whilst(function () {
-    return weightList[n] <= we;
-  }, function (next) {
-    if (weightList[n] == weightList[weightList.length-1]) {
-      weightEqual = weightList[n];
-    } else {
-      weightEqual = weightList[n+1];
-    }
-    n++;
-    next();
-  }, function (err) {
-    // All things are done!
-     });
-  console.log("Округлённый вес: " + weightEqual);
-  // делаем запрос в кладр для поиска региона, нужен номер окато
-  rest.get('http://kladr-api.ru/api.php?query='+ region +'&contentType=region&withParent=1&limit=1&token=' + kladr_token + '&key=' + kladr_key).once('complete', function(kladr) {
-    if (kladr instanceof Error) {
-      console.log('Error: ' + kladr.message);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(req.query.callback + '({error: \'Current carrier is not available!\'})');
-    } else {
-      if (kladr['result'][0] == '') {
-        console.log('Не опознан регион: ' + region);
-        res.setHeader('Content-Type', 'application/json');
-        res.send(req.query.callback + '({error: \'Current carrier is not available!\'})');
-      } else {
-        // идём по массиву данных из output.json и сравниваем есть ли нужный регион
-        async.eachSeries(jobj, function(item, callback) {
-          if ((item.reg_okato == kladr['result'][0]['okato'])||(item.okato == kladr['result'][0]['okato'])) {
-            console.log('Регион опознан: ' + kladr['result'][0]['name']);
-            price = item.weight[weightEqual.toString().replace('.', ',')][1];
-            // ищем город в кладре, также нужен окато. Город нужен чтобы понять куда доставлять, в областной центр или область
-            rest.get('http://kladr-api.ru/api.php?query='+ city +'&contentType=city&withParent=1&limit=1&token=' + kladr_token + '&key=' + kladr_key).once('complete', function(kladr_city) {
-              if (kladr_city instanceof Error) {
-                console.log('Error: ' + kladr_city.message);
-                res.setHeader('Content-Type', 'application/json');
-                res.send(req.query.callback + '({error: \'Current carrier is not available!\'})');
-              } else if (typeof kladr_city['result'][0] === 'undefined') {
-                callback();
-              } else {
-                if (item.okato == kladr_city['result'][0]['okato']) {
-                  price = item.weight[weightEqual.toString().replace('.', ',')][0];
-                }
-                callback();
-              }
-            });
-          } else {
-            callback();
-          }
-        }, function(err) {
-          if (err) {
-            console.log(err);
-            res.setHeader('Content-Type', 'application/json');
-            res.send(req.query.callback + '({error: \'Current carrier is not available!\'})');
-          } else {
-            // возвращаем цену
-            if (price == '') {
-              res.setHeader('Content-Type', 'application/json');
-              res.send(req.query.callback + '({error: \'Current carrier is not available!\'})');
-            } else {
-              res.setHeader('Content-Type', 'application/json');
-              res.send(req.query.callback + '({delivery_price: ' + price + '})');
-            }
-          }
-        });
-      }
-    }
-  });
-};
-
-// скачиваем файлы с тарифами на доставку
-app.get('/update1', function(req, res){
-  rest.get('http://master-angel.ru/tariffs.csv').once('complete', function(result) {
-    if (result instanceof Error) {
-      console.log('Error:', result.message);
-      this.retry(5000);
-    } else {
-      fs.writeFile(__dirname + '/public/tariffs.csv', result, function(err) {
-        if(err) {
-          console.log(err);
-        } else {
-          rest.get('http://master-angel.ru/cities.csv').once('complete', function(result) {
-            if (result instanceof Error) {
-              console.log('Error:', result.message);
-              this.retry(5000);
-            } else {
-              fs.writeFile(__dirname + '/public/cities.csv', result, function(err) {
-                if(err) {
-                  console.log(err);
-                } else {
-                  fs.appendFile(__dirname + '/public/cities.csv', 'Москва;м\nСанкт-Петербург;п', function (err) {
-                    res.redirect('/update2');
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-    }
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
   });
 });
 
-// здесь мы создаём файл с данными, список городов службы доставки с номерами окато и регионами в которых они находятся. Номера окато берутся из кладра.
-app.get('/update2', function(req, res){
-  var tariffs = []
-    , cities = [];
-  csv()
-  .from.path(__dirname+'/public/tariffs.csv', { delimiter: ';', escape: '"' })
-  .transform( function(row){
-    return row;
-  })
-  .on('record', function(data,index){
-    tariffs.push(data);
-  })
-  .on('end', function(count){
-    csv()
-    .from.path(__dirname+'/public/cities.csv', { delimiter: ';', escape: '"' })
-    .transform( function(row){
-      return row;
-    })
-    .on('record', function(row, index){
-      var j = {};
-      var e = [];
-      async.eachSeries(tariffs, function(i, callback) {
-        if (row[1] == 'м') {
-          j[i[0]] = new Array(parseFloat(i[2]), parseFloat(i[5]));
-          callback();
-        } else if (row[1] == 'п') {
-          j[i[0]] = new Array(parseFloat(i[1]), parseFloat(i[3]));
-          callback();
-        } else if (row[1] == 1) {
-          j[i[0]] = new Array(parseFloat(i[4]), parseFloat(i[5]));
-          callback();
-        } else if (row[1] == 2) {
-          j[i[0]] = new Array(parseFloat(i[6]), parseFloat(i[7]));
-          callback();
-        } else if (row[1] == 3) {
-          j[i[0]] = new Array(parseFloat(i[8]), parseFloat(i[9]));
-          callback();
-        } else if (row[1] == 4) {
-          j[i[0]] = new Array(parseFloat(i[10]), parseFloat(i[11]));
-          callback();
-        } else if (row[1] == 5) {
-          j[i[0]] = new Array(parseFloat(i[12]), parseFloat(i[13]));
-          callback();
-        } else if (row[1] == 6) {
-          j[i[0]] = new Array(parseFloat(i[14]), parseFloat(i[15]));
-          callback();
-        }
-      }, function(err) {
-           e = [row[0].toLowerCase(), row[1], j];
-           cities.push({
-             name: e[0],
-             weight: e[2]
-           });
-         });
-    })
-    .on('end', function(count){
-      var c = [];
-      async.eachSeries(cities, function(i, callback) {
-        rest.get('http://kladr-api.ru/api.php?query='+ i.name +'&contentType=city&withParent=1&limit=1&token=' + kladr_token + '&key=' + kladr_key).once('complete', function(kladr) {
-          if (kladr instanceof Error) {
-            console.log('Error: ' + kladr.message);
-          } else {
-            if (i.name == 'москва') {
-              c.push({
-                name: i.name,
-                kladr_name: kladr['result'][0]['name'].toLowerCase(),
-                okato: kladr['result'][0]['okato'],
-                kladr_regfullname: 'московская область',
-                kladr_regname: 'московская',
-                reg_okato: '46000000000',
-                weight: i.weight
-              });
-            } else if (i.name == 'санкт-петербург') {
-              c.push({
-                name: i.name,
-                kladr_name: kladr['result'][0]['name'].toLowerCase(),
-                okato: kladr['result'][0]['okato'],
-                kladr_regfullname: 'ленинградская область',
-                kladr_regname: 'ленинградская',
-                reg_okato: '41000000000',
-                weight: i.weight
-              });
-            } else {
-              c.push({
-                name: i.name,
-                kladr_name: kladr['result'][0]['name'].toLowerCase(),
-                okato: kladr['result'][0]['okato'],
-                kladr_regfullname: kladr['result'][0]['parents'][0]['name'].toLowerCase() + ' ' + kladr['result'][0]['parents'][0]['type'].toLowerCase(),
-                kladr_regname: kladr['result'][0]['parents'][0]['name'].toLowerCase(),
-                reg_okato: kladr['result'][0]['parents'][0]['okato'],
-                weight: i.weight
-              });
-            }
-            console.log(' done > ' + i.name);
-          }
-          callback();
-        });
-      }, function(err) {
-           if(err) {
-             console.log('A file failed to process');
-           } else {
-             fs.writeFile(__dirname + '/public/output.json', JSON.stringify(c), function(err) {
-               if(err) {
-                 console.log(err);
-               } else {
-                 console.log('The file was saved!');
-                 res.send(200, 'Тарифы успешно обновлены');
-               }
-             });
-           }
-         });
-    })
-    .on('error', function(error){
-      console.log(error.message);
-    });
-  })
-  .on('error', function(error){
-    console.log(error.message);
-  });
-});
-
-// сюда прилетает get запрос от insales, делаем магию и отвечаем.
-app.get('/', function(req, res) {
-  console.dir(req.query);
-  var region = req.query.region.replace(/^[а-яА-Я]{1,10}\s/g, '').toLowerCase();
-  var city = req.query.city.toLowerCase();
-  console.log('Регион : ' + req.query.region + '\nГород : ' + req.query.city + '\nВес : ' + req.query.weight);
-  // это на случай если вдруг будет почтовый индекс, тогда опираемся на индекс. Если его не будет, работаем опираясь на регион и город.
-  if (req.query.zip != 0) {
-    fs.readFile(__dirname + '/public/output.json', 'utf8', function (err, data) {
-      if (err) throw err;
-      rest.get('http://postindexapi.ru/' + req.query.zip + '.json').once('complete', function(zip) {
-        if (zip.error_message) {
-          f(res, req, region, city, data);
-        } else {
-          var reg = zip.region.replace(/\s[а-яА-Я]{1,20}$/g, '').toLowerCase();
-          var cit = zip.city.toLowerCase();
-          if (cit == '') {
-            f(res, req, reg, city, data);
-          } else {
-            f(res, req, reg, cit, data);
-          }
-        }
-      });
-    });
-  } else {
-    fs.readFile(__dirname + '/public/output.json', 'utf8', function (err, data) {
-      if (err) throw err;
-      f(res, req, region, city, data);
-    });
-  }
-});
-
-http.createServer(app).listen(app.get('port'), '127.0.0.1', function() {
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+app.listen(app.get('port'), '127.0.0.1', () => {
+  debug('Express server listening on port ' + this.address().port);
 });
 
 // server with small memory, need manual release
 setInterval(function () {
   global.gc();
-  console.log((process.memoryUsage().rss / 1024 / 1024).toFixed(2) + 'Mb');
 }, 10000);
